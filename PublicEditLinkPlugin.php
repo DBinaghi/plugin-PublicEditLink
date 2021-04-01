ab79e2e9869bdb72eb40b753671ccac5b6b2a715
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-04-01 17:09:35 +0200

    Added functionality for Collections and Files

--- PublicEditLinkPlugin.php
+++ PublicEditLinkPlugin.php
@@ -2,21 +2,9 @@
 
 class PublicEditLinkPlugin extends Omeka_Plugin_AbstractPlugin
 {
-    protected $_hooks = array (
-		'initialize'
-	);
-
 	protected $_filters = array (
 		'public_navigation_admin_bar'
 	);
-
-	/**
-     * Add the translations.
-     */
-    public function hookInitialize()
-    {
-        add_translation_source(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'languages');
-    }
  
 	/**
      * Adds item edit link if user logged in.
@@ -24,23 +12,38 @@ class PublicEditLinkPlugin extends Omeka_Plugin_AbstractPlugin
 	public function filterPublicNavigationAdminBar($navLinks) 
 	{
 		$user = current_user();
-		if (isset($user) && isset(get_view()->item)) {
+		if (isset($user)) {
 			$acl = get_acl();
-			if ($acl->isAllowed($user, get_view()->item, 'edit')) {
-				// Saves copy of last menu item - normally, the logout one - then removes it
-				$lastLink = $navLinks;
-				array_splice($lastLink, 0, -1);
-				array_splice($navLinks, -1);
-				
-				// Creates new menu item, then adds it and finalize with saved last one
-				$element = array(
-					'label' => __('Edit current Item'),
-					'uri' => admin_url('/items/edit/' . metadata('item', 'id'))
-				);
-				$navLinks[] = $element;
-				$navLinks = array_merge($navLinks, $lastLink);
+			if (isset(get_view()->item)) {
+				if ($acl->isAllowed($user, get_view()->item, 'edit')) {
+					$navLinks = $this->updateNavlinks($navLinks, 'Item');
+				}
+			} elseif (isset(get_view()->collection)) {
+				if ($acl->isAllowed($user, get_view()->collection, 'edit')) {
+					$navLinks = $this->updateNavlinks($navLinks, 'Collection');
+				} 	
+			} elseif (isset(get_view()->file)) {
+				if ($acl->isAllowed($user, get_view()->file, 'edit')) {
+					$navLinks = $this->updateNavlinks($navLinks, 'File');
+				}
 			}
 		}
 		return $navLinks;
+	}
+	
+	public function updateNavlinks($navLinks, $type)
+	{
+		// Saves copy of last menu item - normally, the logout one - then removes it
+		$lastLink = $navLinks;
+		array_splice($lastLink, 0, -1);
+		array_splice($navLinks, -1);
+		
+		// Creates new menu item, then adds it and finalize with saved last one
+		$element = array(
+			'label' => __('Edit') . ' ' . __($type),
+			'uri' => admin_url('/' . strtolower($type) . 's/edit/' . metadata(strtolower($type), 'id'))
+		);
+		$navLinks[] = $element;
+		return array_merge($navLinks, $lastLink);
 	}
 }
